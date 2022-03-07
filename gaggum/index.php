90f9adf4c360f23ec17f4294e53c46dcfba2865d
YUDAEUN <udu5771@naver.com>
2022-03-08 02:45:33 +0900

    add index.php and myhome index.php

---
+++ gaggum/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>가꿈</title>
+    <style>
+        body {
+        height: 100vh;
+        background-image: url('image/main_image.svg');
+        background-repeat : no-repeat;
+        background-size : cover;
+      }
+    </style>
+</head>
+<body>
+
+<script>
+    setTimeout('move_page()',2000); //3초 후에 페이지 이동
+
+    function move_page(){
+        location.href="page01_myhome/"; //마이홈 페이지로 이동
+    }
+</script>
+</body>
+</html>
---
+++ gaggum/page01_myhome/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>가꿈</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dongle&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Dongle', sans-serif;
+}
+        body {
+            background-image: url('../image/myhome/myhome.svg');
+            margin: auto;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        #total-menu-icon{
+            text-align: center;
+            top: 750px;
+            position: relative;
+            
+        }
+        .ticket-icon{
+            text-align: center;
+            left: 75%;
+            top: 50px;
+            position: fixed;
+            color: #fff;
+        }
+        
+    </style>
+    <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.min.js"></script>
+</head>
+<body>
+<div class="ticket-icon" style=" float: left;">
+        <img src="../image/Ticket.png"> 
+        <div style="float: right; font-size: 26px; line-height:1.0em; color: #fff; margin-left: 2px;"> 380 </div></div>
+
+    <div id="total-menu-icon" >
+    <img src="../image/total_menu_icon.png" alt="">
+    </div>
+    
+</body>
+</html>
